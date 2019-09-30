7e38e99acf7d732029d4eaa14ae09e23a5939924
JamGrif <40271350+JamGrif@users.noreply.github.com>
2019-09-30 17:02:44 +0100

    3

--- 2.5DynamicMemoryAllocation/2.5DynamicMemoryAllocation/2.5DynamicMemoryAllocation.cpp
+++ 2.5DynamicMemoryAllocation/2.5DynamicMemoryAllocation/2.5DynamicMemoryAllocation.cpp
@@ -4,12 +4,8 @@ using namespace std;
 // * is deference
 // & the address of variable
 
-
-int AddInts(int a, int b) 
-{
-	return a + b;
-}
-
+void passByValue(int x);
+void passByReference(int* x);
 
 int main()
 {
@@ -18,13 +14,28 @@ int main()
 	int* address;
 	address = &fish; //memory address of fish
 	dog = *address; //dog contains the value of the memory address of fish
+	cout << dog << endl; //dog should be equal to fish (so 5)
 
-	cout << dog << endl;
+	int value = 13;
+	int reference = 13;
 
+	passByValue(value);
+	passByReference(&reference);
 
+	cout << "value is: " << value << endl;
+	cout << "reference is: " << reference << endl;
 
 }
 
+void passByValue(int x) //Pass copy. X is a copy of value variable
+{
+	x = 99; 
+}
+
+void passByReference(int* x) //Take memory address of variable and directly change it
+{
+	*x = 66;
+}
 
 
 
